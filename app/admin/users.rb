507ff456ae1a49c4845caede43a5b11ee4dff158
Robert Olendorf <drolendorf@gmail.com>
2020-10-14 12:09:33 +0000

    intial user admin views done

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,25 +1,29 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register User do
-  permit_params :email, :password, :password_confirmation
+  permit_params :role
 
   index do
     selectable_column
-    id_column
+    column :avatar_name
+    column :role
     column :current_sign_in_at
     column :sign_in_count
     column :created_at
+    column :updated_at
     actions
   end
-
+  
+  filter :avatar_name
+  filter :role
   filter :current_sign_in_at
   filter :sign_in_count
   filter :created_at
+  filter :updated_at
 
   form do |f|
     f.inputs do
-      f.input :password
-      f.input :password_confirmation
+      f.input :role, include_blank: false
     end
     f.actions
   end

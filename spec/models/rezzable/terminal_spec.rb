ea8130094a6d98e8f0cccb4ca1ad9e905b33a2f8
Robert Olendorf <drolendorf@gmail.com>
2021-01-06 18:14:40 +0000

    fixing failing test

--- spec/models/rezzable/terminal_spec.rb
+++ spec/models/rezzable/terminal_spec.rb
@@ -5,9 +5,11 @@ require 'rails_helper'
 RSpec.describe Rezzable::Terminal, type: :model do
   # it { is_expected.to act_as(:rezzable).class_name('AbstractWebObject') }
   it { should respond_to :abstract_web_object }
+  
+  let(:user) { FactoryBot.create :owner }
 
   let(:terminal) do
-    terminal = FactoryBot.build :terminal
+    terminal = FactoryBot.build :terminal, user_id: user.id
     terminal.save
     terminal
   end

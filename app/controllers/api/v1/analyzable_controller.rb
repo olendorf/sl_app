f80fe8fbe739368ebd01ca01bf4b861c7baaf9f9
Robert Olendorf <drolendorf@gmail.com>
2021-02-25 23:52:09 +0000

    inventories api done

---
+++ app/controllers/api/v1/analyzable_controller.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    # Parent controll for all rezzable objects
+    class AnalyzableController < Api::V1::ApiController
+      
+      skip_before_action :load_requesting_object, except: [:create]
+      prepend_before_action :load_requesting_object
+      
+      
+      private
+      
+      def atts 
+        JSON.parse(request.raw_post)
+      end 
+      
+      def api_key
+        @requesting_object.api_key
+      end
+    end
+  end 
+end 

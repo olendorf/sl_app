43dbc7911ddf5dae6d45bc5b27769263ad3b1296
Robert Olendorf <drolendorf@gmail.com>
2021-03-11 23:39:16 +0000

    fix code coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -32,6 +32,7 @@ SimpleCov.start do
   add_filter 'app/controllers/concerns/active_admin/rezzable_behavior.rb'
   add_filter 'app/models/rezzable.rb'
   add_filter 'app/controllers/concerns/api/exception_handler.rb'
+  add_filter 'app/services'
 end
 
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration

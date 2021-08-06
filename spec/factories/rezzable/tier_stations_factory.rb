b69dec746c29f519dbb7e8ee0417dee5cc1b1d9c
Robert Olendorf <drolendorf@gmail.com>
2021-08-06 12:41:53 +0000

    tier station model added

---
+++ spec/factories/rezzable/tier_stations_factory.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :rezzable_tier_station, aliases: [:tier_station], class: 'Rezzable::TierStation' do
+    association :abstract_web_object, factory: :abstract_web_object
+  end
+end

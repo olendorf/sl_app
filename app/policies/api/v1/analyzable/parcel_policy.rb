8a24b956b685cd1e9148357bc42add2d9a9a1bf3
Robert Olendorf <drolendorf@gmail.com>
2021-08-06 18:47:22 +0000

    parcel and tier station policies added

---
+++ app/policies/api/v1/analyzable/parcel_policy.rb
@@ -0,0 +1,22 @@
+class Api::V1::Analyzable::ParcelPolicy < ApplicationPolicy
+
+  def create?
+    @user.active?
+  end
+
+  def show?
+    create?
+  end
+
+  def update?
+    create?
+  end
+
+  def index?
+    create?
+  end
+
+  def destroy?
+    create?
+  end
+end
--- app/policies/api/v1/rezzable/parcel_box_policy.rb
+++ app/policies/api/v1/rezzable/parcel_box_policy.rb
@@ -1,5 +1,3 @@
 class Api::V1::Rezzable::ParcelBoxPolicy < Api::V1::RezzablePolicy
-  class Scope < Scope
- 
-  end
+
 end

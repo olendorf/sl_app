9a2fac5c7e1bb66f4806adf1ef261a4b3a0adb97
Robert Olendorf <drolendorf@gmail.com>
2021-10-06 13:09:04 +0000

    added template and moved javascripts handling form sprockets to webpack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,24 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("@popperjs/core")
 
+import "bootstrap"
+
+require("../src/jquery").start()
+// require("../src/popper.min")
+// require("../src/bootstrap.min")
+require("../src/owl")
+require("../src/wow")
+require("../src/validation")
+require("../src/jquery.fancybox")
+require("../src/appear")
+require("../src/scrollbar")
+require("../src/tilt.jquery")
+require("../src/jQuery.style.switcher.min")
+require("../src/plugins")
+require("../src/text_animation")
+require("../src/script")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)

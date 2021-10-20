aa6bf511010880d8ebc0456f5b9a57e11a454283
Robert Olendorf <drolendorf@gmail.com>
2021-10-20 16:50:00 +0000

    fixed some oddly failing tests

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -3,3 +3,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
+
+module.exports = {
+    output: {
+        hashFunction: "xxhash64"
+    }
+};

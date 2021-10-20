347411b77056e64d1232e17857deff807520b1d9
Robert Olendorf <drolendorf@gmail.com>
2021-10-20 17:48:50 +0000

    fixing code coverage

--- spec/workers/user_cleanup_worker_spec.rb
+++ spec/workers/user_cleanup_worker_spec.rb
@@ -11,5 +11,14 @@ RSpec.describe UserCleanupWorker, type: :worker do
       described_class.perform_async
       assert_equal 'default', described_class.queue
     end
+    
+    it 'goes into the jobs array for testing environment' do
+      expect do
+        described_class.perform_async
+      end.to change(described_class.jobs, :size).by(1)
+      described_class.new.perform
+    end
   end
+  
+
 end

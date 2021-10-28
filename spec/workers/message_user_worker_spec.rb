4bb981d4b67ce2173b0c51bf66b32e1373626765
Robert Olendorf <drolendorf@gmail.com>
2021-10-28 13:27:13 +0000

    should have improved code coverage

--- spec/workers/message_user_worker_spec.rb
+++ spec/workers/message_user_worker_spec.rb
@@ -14,24 +14,25 @@ RSpec.describe MessageUserWorker, type: :worker do
     %r{\Ahttps://sim3015.aditi.lindenlab.com:12043/cap/[-a-f0-9]{36}/message_user\?
        auth_digest=[a-f0-9]+&auth_time=[0-9]+\z}x
   end
+  let(:uuid) { SecureRandom.uuid }
 
   describe 'queing the job' do
     it 'job in correct queue' do
       stub_request(:post, uri_regex)
         .with(body:
-        /"avatar_name":.*Random Citizen.*Your SLapp Data account expired .*maps.secondlife.com.*/)
-      described_class.perform_async(owner.servers.sample.id, 'Random Citizen', SecureRandom.uuid, 3.days.ago)
+        "{\"avatar_name\":\"Random Citizen\",\"avatar_key\":\"#{uuid}\",\"message\":\"foo\"}")
+      described_class.perform_async(owner.servers.sample.id, 'Random Citizen', uuid, 'foo')
       assert_equal 'default', described_class.queue
     end
 
     it 'goes into the jobs array for testing environment' do
       stub_request(:post, uri_regex)
         .with(body:
-        /"avatar_name":.*Random Citizen.*Your SLapp Data account expired .*maps.secondlife.com.*/)
-      expect do
-        described_class.perform_async(owner.servers.sample.id, 'Random Citizen', SecureRandom.uuid, 3.days.ago)
-      end.to change(described_class.jobs, :size).by(1)
-      described_class.new.perform(owner.servers.sample.id, 'Random Citizen', SecureRandom.uuid, 3.days.ago)
+        "{\"avatar_name\":\"Random Citizen\",\"avatar_key\":\"#{uuid}\",\"message\":\"foo\"}")
+      expect{
+        described_class.perform_async(owner.servers.sample.id, 'Random Citizen', uuid, 'foo')
+      }.to change{ described_class.jobs.size}.by(1)
+      described_class.new.perform(owner.servers.sample.id, 'Random Citizen', uuid, 'foo')
     end
   end
 end
--- spec/workers/process_users_worker_spec.rb
+++ spec/workers/process_users_worker_spec.rb
@@ -6,6 +6,12 @@ require 'rails_helper'
 Sidekiq::Testing.fake!
 
 RSpec.describe ProcessUsersWorker, type: :worker do
+  before(:each) do 
+    owners = FactoryBot.create_list(:owner, 3)
+    3.times do
+      owners.sample.web_objects << FactoryBot.create(:server)
+    end
+  end
   describe 'queing the job' do
     it 'job in correct queue' do
       described_class.perform_async

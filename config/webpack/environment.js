d9f3dd0d67796ec4c446d606b2d4504d1fa888d4
Ubuntu <ubuntu@ip-172-31-2-61.us-east-2.compute.internal>
2022-03-04 18:24:46 +0000

    setup new dev environment

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,23 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
-
-
-const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    'window.jQuery': 'jquery',
-    Popper: ['popper.js', 'default']
-  })
-)
-
-environment.config.merge(
-  {
-    output: {
-        hashFunction: "sha256"
-    }
-}
-)
-
 module.exports = environment

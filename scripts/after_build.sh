074cbce36c84a10674c9652581210b29f09c066f
Robert Olendorf <drolendorf@gmail.com>
2022-04-18 19:19:27 +0000

    added more to after build script

--- scripts/after_build.sh
+++ scripts/after_build.sh
@@ -2,6 +2,12 @@
 
 sudo apt-get install make gcc libpq-dev build-essential g++
 
+cd /var/www/slapp_data
+
+sudo chown -R ubuntu:ubuntu .
+
+EDITOR="vim --wait" rails credentials:edit
+
 sudo gem install bundler -N
 
 sudo bundle install

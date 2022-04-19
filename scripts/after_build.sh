1e1b2f6b11d1927b45153ec2173d023c464b9bb5
Robert Olendorf <drolendorf@gmail.com>
2022-04-19 18:05:37 +0000

    more work on deploy

--- scripts/after_build.sh
+++ scripts/after_build.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+
+cd /var/www/slapp_data
+
+sudo chown -R ubuntu:ubuntu .
+
 sudo apt-get install make gcc libpq-dev build-essential g++
 sudo apt install nodejs npm
 
 npm install --global yarn
 
-cd /var/www/slapp_data
-
-sudo chown -R ubuntu:ubuntu .
 
 
 yarn add bootstrap jquery popper.js

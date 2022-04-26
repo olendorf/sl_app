76835674c81747230d05b77e9c2d6014afc2d460
Robert Olendorf <drolendorf@gmail.com>
2022-04-26 13:16:18 +0000

    fixing after build

--- scripts/after_build.sh
+++ scripts/after_build.sh
@@ -5,7 +5,7 @@ cd /var/www/slapp_data
 
 sudo chown -R ubuntu:ubuntu .
 sudo apt-get update -y
-sudo apt-get -y install make gcc libpq-dev build-essential g++
+sudo apt-get -y libpq-dev build-essential g++
 sudo apt -y install nodejs npm
 sudo npm install -g n
 sudo n 12.0.0
@@ -14,15 +14,17 @@ sudo npm install --global yarn
 
 
 
-yarn add bootstrap jquery popper.js
-
-EDITOR="vim --wait" rails credentials:edit
+yarn add bootstrap jquery @popperjs/core
 
 
 gem install bundler -N
 
 
+
 bundle install
+
+
+EDITOR="vim --wait" rails credentials:edit
 
 RAILS_ENV=staging rails assets:precompile
 

e76f4c731fa67c8ce41c020ecedefb64e0514990
Robert Olendorf <drolendorf@gmail.com>
2022-04-28 19:10:00 +0000

    working on after_build

--- scripts/after_build.sh
+++ scripts/after_build.sh
@@ -1,37 +1,51 @@
 #!/bin/bash
 
+echo "export USERNAME=postgres" >> /home/ubuntu/.profile
+echo "export PASSWORD=password" >> /home/ubuntu/.profile
+echo "export DB_IP=slapp-data-dev.cbw3mio5zyeu.us-east-2.rds.amazonaws.com" >> /home/ubuntu/.profile
 
-cd /var/www/slapp_data
+source /home/ubuntu/.profile
 
-sudo chown -R ubuntu:ubuntu .
-sudo apt-get update -y
-sudo apt-get -y libpq-dev build-essential g++
-sudo apt -y install nodejs npm
-sudo npm install -g n
-sudo n 12.0.0
+# cd /var/www/slapp_data
 
-sudo npm install --global yarn
+# sudo chown -R ubuntu:ubuntu .
+# sudo apt-get update -y
+# sudo apt-get -y libpq-dev build-essential g++
+# sudo apt -y install nodejs npm
+# sudo npm install -g n
+# sudo n 12.0.0
 
-
-
-yarn add bootstrap jquery @popperjs/core
-
-whoami
-pwd
-ls -al 
-
-echo 'export GEM_HOME=~/.ruby/' >> ~/.bashrc
-echo 'export PATH="$PATH:~/.ruby/bin"' >> ~/.bashrc
-source ~/.bashrc
-
-gem install bundler -N
+# sudo npm install --global yarn
 
 
 
-bundle install
+# yarn add bootstrap jquery @popperjs/core
+
+# sudo apt purge -y ruby
+
+# sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libxml2-dev libxslt1-dev libcurl4-openssl-dev libffi-dev
+
+# git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+# echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+# echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+# exec $SHELL
+
+# git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+# echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+# exec $SHELL
+
+# rbenv install 2.3.1
+# rbenv global 2.3.1
+# ruby -v
+
+# gem install bundler -N
 
 
-EDITOR="vim --wait" rails credentials:edit
 
-RAILS_ENV=staging rails assets:precompile
+# bundle install
+
+
+# EDITOR="vim --wait" rails credentials:edit
+
+# RAILS_ENV=staging rails assets:precompile
 

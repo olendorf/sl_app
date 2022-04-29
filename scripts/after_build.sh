fcdfb6018bbc2920978bd4eca49bbcbe2a9d1f1a
Robert Olendorf <drolendorf@gmail.com>
2022-04-29 18:47:12 +0000

    still figuring out after build

--- scripts/after_build.sh
+++ scripts/after_build.sh
@@ -20,8 +20,7 @@ curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer
 
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-source ~/.bashrc
-
+source /home/ubuntu/.bashrc
 
 rbenv install 2.6.3
 rbenv global 2.6.3

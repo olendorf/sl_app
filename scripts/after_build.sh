103b63c209da8a1391631deae8caa2232c61f1d0
Robert Olendorf <drolendorf@gmail.com>
2022-05-02 20:00:59 +0000

    adding apache and passenger install

--- scripts/after_build.sh
+++ scripts/after_build.sh
@@ -31,31 +31,31 @@ rbenv global 2.6.3
 
 exec bash
 
-sudo apt install -y apache2
+# sudo apt install -y apache2
 
-sudo ufw allow 'Apache Full'
-sudo ufw allow 'OpenSSH'
+# sudo ufw allow 'Apache Full'
+# sudo ufw allow 'OpenSSH'
 
-sudo apt-get install -y dirmngr gnupg
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7
-sudo apt-get install -y apt-transport-https ca-certificates
+# sudo apt-get install -y dirmngr gnupg
+# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7
+# sudo apt-get install -y apt-transport-https ca-certificates
 
-sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger bionic main > /etc/apt/sources.list.d/passenger.list'
-sudo apt-get -y update
+# sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger bionic main > /etc/apt/sources.list.d/passenger.list'
+# sudo apt-get -y update
 
-sudo apt-get install -y libapache2-mod-passenger
+# sudo apt-get install -y libapache2-mod-passenger
 
-sudo a2enmod passenger
-sudo apache2ctl restart
+# sudo a2enmod passenger
+# sudo apache2ctl restart
 
-gem install bundler -N
-
-
-
-bundle install
+# gem install bundler -N
 
 
-EDITOR="vim --wait" rails credentials:edit
 
-RAILS_ENV=staging rails assets:precompile
+# bundle install
+
+
+# EDITOR="vim --wait" rails credentials:edit
+
+# RAILS_ENV=staging rails assets:precompile
 

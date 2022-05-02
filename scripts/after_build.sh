04d6960bf31cda41bfbcefd3389989300605dfc5
Robert Olendorf <drolendorf@gmail.com>
2022-05-02 19:20:40 +0000

    adding apache and passenger install

--- scripts/after_build.sh
+++ scripts/after_build.sh
@@ -41,7 +41,7 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CA
 sudo apt-get install -y apt-transport-https ca-certificates
 
 sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger bionic main > /etc/apt/sources.list.d/passenger.list'
-sudo apt-get update
+sudo apt-get -y update
 
 sudo apt-get install -y libapache2-mod-passenger
 

d77623dd5a240ba9ee5451931e239a6927eef190
Robert Olendorf <drolendorf@gmail.com>
2022-05-17 18:30:18 +0000

    figuring stuff out

--- app/models/rezzable/web_object.rb
+++ app/models/rezzable/web_object.rb
@@ -4,9 +4,6 @@ module Rezzable
   # Mostly a test for Abstract Web Objects.
   class WebObject < ApplicationRecord
     acts_as :abstract_web_object
-    
-    def api_key
-      Settings.default.web_object.api_key
-    end
+
   end
 end

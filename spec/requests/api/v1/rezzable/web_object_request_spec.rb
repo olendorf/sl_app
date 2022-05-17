ac93bf88efa9d82d1252f8f6d6378c6ac1c0faba
Robert Olendorf <drolendorf@gmail.com>
2022-05-17 19:58:10 +0000

    fixed failing webobject test

--- spec/requests/api/v1/rezzable/web_object_request_spec.rb
+++ spec/requests/api/v1/rezzable/web_object_request_spec.rb
@@ -9,8 +9,7 @@ RSpec.describe 'Api::V1::Rezzable::WebObjects', type: :request do
       FactoryBot.create :web_object, user_id: user.id
   }
   let(:path) { api_rezzable_web_object_path(web_object.object_key) }
-  before(:each) { get path, headers: headers(web_object, 
-                              api_key: Settings.default.web_object.api_key) }
+  before(:each) { get path, headers: headers(web_object) }
   it 'should return OK status' do
     expect(response.status).to eq 200
   end
